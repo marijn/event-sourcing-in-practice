0b9fdafbf8c3b4ba61585719638176f6edc620da
Marijn Huizendveld <marijn@huizendveld.com>
2020-11-16 18:53:27 +0100

    :construction: Identify element.

---
+++ src/Infra/EventSourcing/DomainQuery.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Infra\EventSourcing;
+
+/**
+ * @copyright Marijn Huizendveld 2020. All rights reserved.
+ */
+interface DomainQuery extends Message {
+
+}
