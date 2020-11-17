50caa736caca50f525122aef603a8e4f7ceb5a6e
Marijn Huizendveld <marijn@round84.com>
2020-11-17 21:29:20 +0100

    :construction: Identify type for `ProcessManager`.

---
+++ src/Infra/EventSourcing/ProcessManager/ProcessManager.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Infra\EventSourcing\ProcessManager;
+
+/**
+ * @copyright Marijn Huizendveld 2020. All rights reserved.
+ */
+interface ProcessManager {
+
+}
